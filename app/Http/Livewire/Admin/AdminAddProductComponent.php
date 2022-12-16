43a9e1c47055ea0cffde203462796fed3575cc33
emad-hash <emadhashash76@gmail.com>
2022-12-16 19:46:37 +0300

    product page

--- app/Http/Livewire/Admin/AdminAddProductComponent.php
+++ app/Http/Livewire/Admin/AdminAddProductComponent.php
@@ -60,7 +60,7 @@ class AdminAddProductComponent extends Component
         $product->image = $imageName;
         $product->category_id = $this->category_id;
         $product->save();
-        session()->flash('message', 'Product has been created successfully !');
+        return redirect("/admin/products")->session()->flash('message', 'Product has been created successfully !');
     }
 
     public function render()
